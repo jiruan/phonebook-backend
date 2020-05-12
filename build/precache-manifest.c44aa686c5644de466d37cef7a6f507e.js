9f1100ecd761078f12743a509ce73d83cbee958a
jiruan <jiruan@umich.edu>
2020-05-12 00:05:04 -0500

    new build

---
+++ build/precache-manifest.c44aa686c5644de466d37cef7a6f507e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd31848c3ee4a562055c3a9e9794bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07de261a9e030eac929c",
+    "url": "/static/css/main.d8c60688.chunk.css"
+  },
+  {
+    "revision": "b28415f9e580a41a905f",
+    "url": "/static/js/2.4be49b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4be49b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de261a9e030eac929c",
+    "url": "/static/js/main.4bdb058a.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
---
+++ build/precache-manifest.d2b56fdb9307c4f42bb8ff9429051166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2c1fe8cd476cebd758f917e5d9aa41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fab8cf6cd70f84e115d",
+    "url": "/static/css/main.d8c60688.chunk.css"
+  },
+  {
+    "revision": "b28415f9e580a41a905f",
+    "url": "/static/js/2.4be49b41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4be49b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fab8cf6cd70f84e115d",
+    "url": "/static/js/main.bd70adf2.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
